12f5f4b63e023aa013bd1395fa999d4bb8adb10c
clia <clia@163.com>
2018-11-03 20:37:34 +0800

    Reduce unsafe usage range

--- examples/pgxr_example_query/src/lib.rs
+++ examples/pgxr_example_query/src/lib.rs
@@ -13,22 +13,20 @@ PG_FUNCTION_INFO_V1!(pg_finfo_pgxr_example_query);
 pub extern "C" fn pgxr_example_query(_fcinfo: FunctionCallInfo) -> Datum
 {
     let mut result: Datum = 0;
-    unsafe {
-        SPI_connect();
-        let sql = "select 2";
-        let null_terminated = CString::new(sql).unwrap();
-        let ret = SPI_execute(null_terminated.as_ptr(), true as bool_, 0);
-	    let proc = SPI_processed;
-        if ret == SPI_OK_SELECT as i32 && proc > 0 {
-            let spi_tuptable = *SPI_tuptable;
-            let spi_tupdesc = spi_tuptable.tupdesc;
-            let vals = slice::from_raw_parts(spi_tuptable.vals, proc as usize);
-            for i in 0..proc {
-                let spi_tuple = vals[i as usize];
-                result = SPI_getbinval(spi_tuple, spi_tupdesc, 1, &mut (false as bool_));
-            }
+    unsafe { SPI_connect(); }
+    let sql = "select 2";
+    let null_terminated = CString::new(sql).unwrap();
+    let ret = unsafe { SPI_execute(null_terminated.as_ptr(), true as bool_, 0) };
+    let proc = unsafe { SPI_processed };
+    if ret == SPI_OK_SELECT as i32 && proc > 0 {
+        let spi_tuptable = unsafe { *SPI_tuptable };
+        let spi_tupdesc = spi_tuptable.tupdesc;
+        let vals = unsafe { slice::from_raw_parts(spi_tuptable.vals, proc as usize) };
+        for i in 0..proc {
+            let spi_tuple = vals[i as usize];
+            result = unsafe { SPI_getbinval(spi_tuple, spi_tupdesc, 1, &mut (false as bool_)) };
         }
-        SPI_finish();
     }
+    unsafe { SPI_finish(); }
     result
 }
