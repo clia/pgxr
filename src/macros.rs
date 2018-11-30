b21dc3c22347b0c67647141b5f1215375ccf012c
clia <clia@163.com>
2018-11-30 16:01:15 +0800

    Fix macros

--- src/macros.rs
+++ src/macros.rs
@@ -20,7 +20,7 @@ macro_rules! PG_MODULE_MAGIC {
 
 #[macro_export]
 macro_rules! PG_FUNCTION_INFO_V1 {
-    ( $funcname:ident ) => {
+    ($funcname:ident) => {
         #[no_mangle]
         pub extern "C" fn $funcname() -> *const Pg_finfo_record
         {
@@ -38,9 +38,9 @@ macro_rules! PG_FUNCTION_INFO_V1 {
 /// While returning String
 #[macro_export]
 macro_rules! try_return_string {
-    ( $expr:expr ) => (match $expr {
-        $crate::result::Result::Ok(val) => val,
-        $crate::result::Result::Err(err) => {
+    ($expr:expr) => (match $expr {
+        std::result::Result::Ok(val) => val,
+        std::result::Result::Err(err) => {
             return PG_RETURN_STRING(format!("ERROR: {}", err.description()))
         }
     });
@@ -49,9 +49,9 @@ macro_rules! try_return_string {
 /// While returning integer
 #[macro_export]
 macro_rules! try_return_int {
-    ( $expr:expr ) => (match $expr {
-        $crate::result::Result::Ok(val) => val,
-        $crate::result::Result::Err(err) => {
+    ($expr:expr) => (match $expr {
+        std::result::Result::Ok(val) => val,
+        std::result::Result::Err(err) => {
             return PG_RETURN_I32(-1)
         }
     });
